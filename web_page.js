a4f018467c267f0684d55acfc080ee2032841c94
SHIN-sped <sml2031@gmail.com>
2023-03-09 01:43:46 +0000

    name

--- web_page.js
+++ web_page.js
@@ -1,7 +1,8 @@
 let btn = document.getElementById('btn');
 btn.addEventListener('click',() =>{
     alert("나 눌렀어?")
-})    
+})
+    
 let red_btn = document.getElementById('red_btn');
 let content = document.getElementById('content');
 
